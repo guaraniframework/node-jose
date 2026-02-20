faf6164fa1e974cd4a4f9892c91685c9ce69b9ba
Revensky <38702533+revensky@users.noreply.github.com>
2026-02-19 21:30:32 -0300

    feat(jwa): add jwa types

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// JSON Web Algorithms
+export { type JweAlg } from './lib/jwa/jwe/jwe-alg.type';
+export { type JweEnc } from './lib/jwa/jwe/jwe-enc.type';
+export { type JweZip } from './lib/jwa/jwe/jwe-zip.type';
+export { type JwkCrv } from './lib/jwa/jwk/jwk-crv.type';
+export { type JwkKeyOp } from './lib/jwa/jwk/jwk-key-op.type';
+export { type JwkKty } from './lib/jwa/jwk/jwk-kty.type';
+export { type JwkUse } from './lib/jwa/jwk/jwk-use.type';
+export { type JwsAlg } from './lib/jwa/jws/jws-alg.type';
---
+++ src/lib/jwa/jws/jws-alg.type.ts
@@ -0,0 +1,17 @@
+/**
+ * Supported JWS Digital Signature or MAC Algorithms.
+ */
+export type JwsAlg =
+  | 'ES256'
+  | 'ES384'
+  | 'ES512'
+  | 'HS256'
+  | 'HS384'
+  | 'HS512'
+  | 'PS256'
+  | 'PS384'
+  | 'PS512'
+  | 'RS256'
+  | 'RS384'
+  | 'RS512'
+  | 'none';
